293a11e7efd3edc6782e45da743e288a6167e7fd
Per Unneberg <per.unneberg@scilifelab.se>
2020-03-30 23:19:58 +0200

    Update slurm config to allow testing cpus-per-task
    - simplify slurm.conf update
    - Set SelectTypeParameters to CR_Core

--- tests/conftest.py
+++ tests/conftest.py
@@ -82,45 +82,62 @@ def add_slurm_user(user, container):
 
 
 def link_python(container):
-    (exit_code, output) = container.exec_run("which python{}.{}".format(
-        sys.version_info.major,
-        sys.version_info.minor))
+    (exit_code, output) = container.exec_run(
+        "which python{}.{}".format(sys.version_info.major, sys.version_info.minor)
+    )
     cmd = "ln -s {} /usr/bin/python{}".format(output.decode(), sys.version_info.major)
     container.exec_run(cmd, detach=False, stream=False, user="root")
 
 
+SLURM_CONF = """
+# NEW COMPUTE NODE DEFINITIONS
+NodeName=DEFAULT Sockets=1 CoresPerSocket=2 ThreadsPerCore=2 State=UNKNOWN TmpDisk=10000
+NodeName=c[1-2] NodeHostName=localhost NodeAddr=127.0.0.1 RealMemory=500 Feature=thin,mem500MB
+NodeName=c[3-4] NodeHostName=localhost NodeAddr=127.0.0.1 RealMemory=800 Feature=fat,mem800MB
+NodeName=c[5] NodeHostName=localhost NodeAddr=127.0.0.1 RealMemory=500 Feature=thin,mem500MB
+# NEW PARTITIONS
+PartitionName=normal Default=YES Nodes=c[1-4] Shared=NO MaxNodes=1 MaxTime=5-00:00:00 DefaultTime=5-00:00:00 State=UP DefMemPerCPU=500 OverSubscribe=NO
+PartitionName=debug  Nodes=c[5] Shared=NO MaxNodes=1 MaxTime=05:00:00 DefaultTime=05:00:00 State=UP DefMemPerCPU=500
+"""
+
+
 def setup_sacctmgr(container):
-    nodes = "NodeName=c\[1-5\] NodeHostName=localhost NodeAddr=127.0.0.1 RealMemory=1000"
-    nodes2 = "NodeName=c\[6-10\] NodeHostName=localhost NodeAddr=127.0.0.1 RealMemory=1000 Gres=gpu:titanxp:1"
-    newnodes = "\\n".join([
-        "NodeName=c[1-3] NodeHostName=localhost NodeAddr=127.0.0.1 RealMemory=500 Feature=thin,mem500MB",
-        "NodeName=c[4-5] NodeHostName=localhost NodeAddr=127.0.0.1 RealMemory=800 Feature=fat,mem800MB",
-        "NodeName=c[6-10] NodeHostName=localhost NodeAddr=127.0.0.1 RealMemory=500 Feature=thin,mem500MB"
-    ])
-    partition = "PartitionName=debug Nodes=c\[6-10\] Priority=50 DefMemPerCPU=500 Shared=NO MaxNodes=5 MaxTime=5-00:00:00 DefaultTime=5-00:00:00 State=UP"
-    partition_new = partition.replace("5-00", "05")
+    slurmconf = "/etc/slurm/slurm.conf"
     try:
         cmd_args = [
-            "/bin/bash -c 'sacctmgr --immediate add cluster name=linux ; ",
-            "sed -i -e \"s/{old}/{new}/g\" /etc/slurm/slurm.conf ;".format(old=nodes, new=newnodes),
-            "sed -i -e \"s/{old}/#/g\" /etc/slurm/slurm.conf ;".format(old=nodes2),
-            "sed -i -e \"s/{old}/{new}/g\" /etc/slurm/slurm.conf ;".format(old=partition, new=partition_new),
+            "/bin/bash -c 'sacctmgr --immediate add cluster name=linux;",
+            f'sed -i -e "s/CR_CPU_Memory/CR_Core/g" {slurmconf} ;',
+            f'sed -i -e "s/^NodeName/# NodeName/g" {slurmconf} ;',
+            f'sed -i -e "s/^PartitionName/# PartitionName/g" {slurmconf} ;',
+            f'echo "{SLURM_CONF}" >> {slurmconf} ; ',
+            "service supervisor stop;",
+            "service supervisor start;",
             "supervisorctl restart slurmdbd;",
             "supervisorctl restart slurmctld;",
-            "sacctmgr --immediate add account none,test Cluster=linux Description=\"none\" Organization=\"none\"'"
+            'sacctmgr --immediate add account none,test Cluster=linux Description="none" Organization="none"\'',
         ]
         cmd = " ".join(cmd_args)
-        container.exec_run(cmd, detach=False, stream=False, user="root")
+        (exit_code, output) = container.exec_run(
+            f'grep -c "NEW PARTITIONS" {slurmconf}',
+            user="root",
+            detach=False,
+            stream=False,
+        )
+        if int(output.decode().strip()) == 0:
+            logger.info("Setting up slurm partitions")
+            container.exec_run(cmd, detach=False, stream=False, user="root")
     except:
         raise
 
 
 def get_snakemake_quay_tag():
     import requests
+
     try:
         r = requests.get(
-            "https://quay.io/api/v1/repository/biocontainers/snakemake/tag")
-        tags = [t['name'] for t in r.json()['tags']]
+            "https://quay.io/api/v1/repository/biocontainers/snakemake/tag"
+        )
+        tags = [t["name"] for t in r.json()["tags"]]
     except:
         logger.error("couldn't complete requests for quay.io")
         raise
@@ -252,18 +269,21 @@ def data(tmpdir_factory, _cookiecutter_config_file, docker_compose):
     SNAKEFILE.copy(snakefile)
     cluster_config = p.join("cluster-config.yaml")
     CLUSTERCONFIG.copy(cluster_config)
-    template = os.path.join(
-        os.path.abspath(os.path.dirname(__file__)), os.pardir)
+    template = os.path.join(os.path.abspath(os.path.dirname(__file__)), os.pardir)
     output_factory = tmpdir_factory.mktemp
     defaults = "partition=normal output=logs/slurm-%j.out error=logs/slurm-%j.err"
     c = Cookies(template, output_factory, _cookiecutter_config_file)
     c._new_output_dir = lambda: str(p.join("slurm"))
-    c.bake(extra_context={'sbatch_defaults': defaults})
+    c.bake(extra_context={"sbatch_defaults": defaults})
     # Advanced setting
     c = Cookies(template, output_factory, _cookiecutter_config_file)
     c._new_output_dir = lambda: str(p.join("slurm-advanced"))
-    c.bake(extra_context={'sbatch_defaults': defaults,
-                          'advanced_argument_conversion': "True"})
+    c.bake(
+        extra_context={
+            "sbatch_defaults": defaults,
+            "advanced_argument_conversion": "yes",
+        }
+    )
     return p
 
 
